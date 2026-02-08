f0b980985cf89f59677b369a2580db3d76a57d60
vsci6282 <vsci6282@gmail.com>
2026-02-08 16:11:13 -0600

    Update main.py, made snake game ready to be integrated with AI

--- main.py
+++ main.py
@@ -12,14 +12,13 @@ class snake(gym.Env):
     pygame.init()
     isRunning = True
     playSpace = np.zeros((12, 12))
-    playerPos = [6, 6]
-    playerLen = 0
-    playerTail = [[6, 6] for i in range(0, playerLen-1)]
 
-    applePos = [r.randint(0, 11), r.randint(0, 11)]
+    playerDirDict = {1:[0, 1], 2:[-1, 0], 3:[0, -1], 4:[1, 0]}
+    playerActionDirDict = {1:-1, 2:0, 3:1}
 
-    playerDir = 1
     actions = 3
+
+    stepsSinceHadFood = 0
 
     agLeft = 1
     agForward = 2
@@ -28,41 +27,86 @@ class snake(gym.Env):
     empty = 0
     snake = 1
     food = 2
-    def __init__(self, grid_size=12):
+    def __init__(self, hasHumanPlayer):
+
+        self.playerPos = [6, 6]
+        self.playerLen = 0
+        self.playerTail = [[6, 6] for i in range(0, self.playerLen - 1)]
+
+        self.applePos = [r.randint(0, 11), r.randint(0, 11)]
+
+        self.hasHumanPlayer = hasHumanPlayer
+
+        self.playerDir = 1
         if willRender:
             self.screen = pygame.display.set_mode((600, 600))
             self.squareSize = 20
             self.squareMargin = 2
             self.colorDict = {0: (120, 120, 120), 1: (0, 200, 200), 2: (200, 50, 50)}
             self.playSpaceLeftTop = (200, 200)
+
+        self.action_space = spaces.Discrete(3)
+        # The observation space, "position" is the coordinates of the head; "direction" is which way the sanke is heading, "grid" contains the full grid info
+        self.observation_space = gym.spaces.Dict(
+            spaces={
+                "position": gym.spaces.Box(low=0, high=(11), shape=(2,), dtype=np.int32),
+                "direction": gym.spaces.Box(low=-1, high=1, shape=(2,), dtype=np.int32),
+                "grid": gym.spaces.Box(low=0, high=2, shape=(12, 12), dtype=np.uint8),
+            })
         #self.action_space = ... #define possible actions
         #self.observation_space = ... #format of observations
 
     def _get_obs(self):
         """Calculates the observations (input) from current state"""
-        ...
-        #return observation #in format of self.observation_space
+        direction = np.array(self.playerDirDict[self.playerDir])
+        # return observation in the format of self.observation_space
+        return {"position": np.array(self.playerPos, dtype=np.int32),
+                "direction": direction.astype(np.int32),
+                "grid": self.playSpace}
+
     def reset(self):
-        """Resetting the environment (i.e., starting a new game)"""
-        ...
+        self.playerPos = [6, 6]
+        self.playerLen = 0
+        self.playerTail = [[6, 6] for i in range(0, self.playerLen - 1)]
+
+        self.applePos = [r.randint(0, 11), r.randint(0, 11)]
+        while self.applePos in self.playerTail + self.playerPos:
+            self.applePos = [r.randint(0, 11), r.randint(0, 11)]
+
+        self.playerDir = 1
+        playSpace = np.zeros((12, 12))
         return self._get_obs()
 
     def step(self, action):
         """Evolve environment in response to action and calculate reward"""
+
         print(f'applePos: {self.applePos}')
         print(f'playerPos: {self.playerPos}')
         print(f'snakeLen: {self.playerLen}')
         print(f'playertail: {self.playerTail}')
         print(len(self.playerTail))
+
+        done = False
+        reward = 0
+        preDistToFood = self.getDistToFood()
+
         for pos in self.playerTail:
             if pos == self.playerPos:
-                pygame.quit()
+                done = True
+                reward -= 20
+
+        if self.stepsSinceHadFood >= 200:
+            done = True
+
+        if self.playerLen >= 143:
+            done = True
+
         self.playSpace[self.applePos[0], self.applePos[1]] = 2
+
         if len(self.playerTail) < 0:
             self.playSpace[self.playerPos[0], self.playerPos[1]] = 0
         elif len(self.playerTail) < self.playerLen:
             self.playerTail.append([self.playerPos[0], self.playerPos[1]])
-            print('append')
         elif len(self.playerTail) == self.playerLen:
             self.playerTail.append([self.playerPos[0], self.playerPos[1]])
             toBeRemoved = self.playerTail[0].copy()
@@ -72,38 +116,40 @@ class snake(gym.Env):
             toBeRemoved = self.playerTail[0].copy()
             self.playerTail.pop(0)
             self.playSpace[toBeRemoved[0], toBeRemoved[1]] = 0
-        if action == 1:
-            self.playerDir -= 1
-        elif action == 2:
-            pass
-        elif action == 3:
-            self.playerDir += 1
+
+        self.playerDir += self.playerActionDirDict[action]
         self.playerDir = (self.playerDir-1)%4+1
-        if self.playerDir == 1:
-            self.playerPos[1] += 1
-        if self.playerDir == 2:
-            self.playerPos[0] -= 1
-        if self.playerDir == 3:
-            self.playerPos[1] -= 1
-        if self.playerDir == 4:
-            self.playerPos[0] += 1
+
+        self.playerPos[0] += self.playerDirDict[self.playerDir][0]
+        self.playerPos[1] += self.playerDirDict[self.playerDir][1]
+
+        postDistToFood = np.abs(self.playerPos[0]-self.applePos[0])
+
+        if preDistToFood >= postDistToFood:
+            reward += 1
+        else:
+            reward -= 1
 
         if self.playerPos == self.applePos:
+            reward += 50
+            self.stepsSinceHadFood = 0
             print("Father, I have felt the apple perish and rot within my gaping maw. Still, my hunger persists. I desire more.")
             self.playerLen += 1
             self.applePos = [r.randint(0, 11), r.randint(0, 11)]
             while self.applePos in self.playerTail + self.playerPos:
                 self.applePos = [r.randint(0, 11), r.randint(0, 11)]
+        else:
+            self.stepsSinceHadFood += 1
 
         if 0 <= self.playerPos[0] <= 11 and 0 <= self.playerPos[1] <= 11:
-            self.playSpace[self.playerPos[0], self.playerPos[1]] = 1
-            for pos in self.playerTail:
-                pass
+            for pos in self.playerTail+[self.playerPos]:
                 self.playSpace[pos[0], pos[1]] = 1
         else:
             print('goodbye, cruel world')
-            pygame.quit()
-        #return  self._get_obs(), reward, done, info
+            done = True
+            reward -= 20
+
+        return  self._get_obs(), reward, done, {}
 
     def render(self):
         for coords, value in np.ndenumerate(self.playSpace):
@@ -111,6 +157,9 @@ class snake(gym.Env):
                                         self.playSpaceLeftTop[1] + (coords[1] * (self.squareSize + self.squareMargin)),
                                         self.squareSize, self.squareSize)
             pygame.draw.rect(self.screen, self.colorDict[value], rectToBeDrawn)
+
+    def getDistToFood(self):
+        return np.abs(self.playerPos[0]-self.applePos[0]) + np.abs(self.playerPos[1]-self.applePos[1])
 '''
 def render():
     for coords, value in np.ndenumerate(playSpace):
@@ -144,9 +193,9 @@ def updatePlaySpace():
 
 '''
 running = True
-game = snake()
+game = snake(True)
 act = 0
-while running:
+while running and game.hasHumanPlayer:
     act = 2
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
@@ -159,10 +208,12 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     game.screen.fill("black")
-    game.step(act)
+    info = game.step(act)
+    if info[2]:
+        running = False
     game.render()
     pygame.display.flip()
 
     t.sleep(0.3)
 
-pygame.quit()
+#pygame.quit()
